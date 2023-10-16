88edbdf83bd90cef77f45e2adc5d6576950739b3
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-10-16 16:52:27 +0900

    [RENAME] : ✏️ mypage to exercise_condition

--- app/src/main/java/com/example/alldayfit/ExerciseConditionFragment.kt
+++ app/src/main/java/com/example/alldayfit/ExerciseConditionFragment.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.alldayfit.databinding.MyPageFragmentBinding
+import com.example.alldayfit.databinding.ExerciseConditionFragmentBinding
 
 
-class MyPageFragment : Fragment() {
+class ExerciseConditionFragment : Fragment() {
 
-    private var _binding: MyPageFragmentBinding? = null
+    private var _binding: ExerciseConditionFragmentBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +22,7 @@ class MyPageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = MyPageFragmentBinding.inflate(inflater, container, false)
+        _binding = ExerciseConditionFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
