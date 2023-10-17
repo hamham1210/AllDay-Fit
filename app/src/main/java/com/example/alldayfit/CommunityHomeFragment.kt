60fcfb763a98b1fff5829d4b05e6d6c5c4cc26b6
yeonjooRyu <yeonjoo3751@gmail.com>
2023-10-17 20:19:03 +0900

    커뮤니티 UI Design

--- app/src/main/java/com/example/alldayfit/CommunityHomeFragment.kt
+++ app/src/main/java/com/example/alldayfit/CommunityHomeFragment.kt
@@ -5,20 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.alldayfit.databinding.CalendarFragmentBinding
+import com.example.alldayfit.databinding.CommunityHomeFragmentBinding
 
 class CommunityHomeFragment : Fragment() {
+    private var _binding: CommunityHomeFragmentBinding? = null
+    private val binding get() = _binding!!
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = CommunityHomeFragmentBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.community_home_fragment, container, false)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
-
-
 }
