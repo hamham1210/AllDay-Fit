6b7368886f5b712b2157d7064e1cd1bfc7bdd9de
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-10-18 09:55:02 +0900

    [REFACTOR] : ♻️ rename file

--- app/src/main/java/com/example/alldayfit/ExerciseStatusFragment.kt
+++ app/src/main/java/com/example/alldayfit/ExerciseStatusFragment.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.alldayfit.databinding.ExerciseConditionFragmentBinding
+import com.example.alldayfit.databinding.ExerciseStatusFragmentBinding
 
 
-class ExerciseConditionFragment : Fragment() {
+class ExerciseStatusFragment : Fragment() {
 
-    private var _binding: ExerciseConditionFragmentBinding? = null
+    private var _binding: ExerciseStatusFragmentBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +22,7 @@ class ExerciseConditionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = ExerciseConditionFragmentBinding.inflate(inflater, container, false)
+        _binding = ExerciseStatusFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
