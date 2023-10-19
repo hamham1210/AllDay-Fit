60874a74dbddc8a68049a0eaf463f08f11d95d13
iWantBigMan <skarndgus8899@gmail.com>
2023-10-20 06:44:15 +0900

    [FEAT] : ⚙️ 캘린더 테스트용

--- app/src/main/java/com/example/alldayfit/calendar/CalendarActivity.kt
+++ app/src/main/java/com/example/alldayfit/calendar/CalendarActivity.kt
@@ -1,23 +1,52 @@
 package com.example.alldayfit.calendar
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.alldayfit.R
 import com.example.alldayfit.databinding.CalendarActivityBinding
 
 
+
 class CalendarActivity : AppCompatActivity() {
 
-    private lateinit var binding: CalendarActivityBinding
+    internal lateinit var binding: CalendarActivityBinding
+
+    private val examplesAdapter = CalendarViewOptionsAdapter {
+        val fragment = it.createView()
+        val anim = it.animation
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(anim.enter, anim.exit, anim.popEnter, anim.popExit)
+            .add(R.id.homeContainer, fragment, fragment.javaClass.simpleName)
+            .addToBackStack(fragment.javaClass.simpleName)
+            .commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = CalendarActivityBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
+        setSupportActionBar(binding.activityToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.examplesRecyclerview.apply {
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            adapter = examplesAdapter
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+        }
+    }
 
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.calendarFrame, CalendarFragment())
-        fragmentTransaction.addToBackStack(null)
-        fragmentTransaction.commit()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> onBackPressed().let { true }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
+
+//        val fragmentTransaction = supportFragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.calendarFrame, CalendarFragment())
+//        fragmentTransaction.addToBackStack(null)
+//        fragmentTransaction.commit()
