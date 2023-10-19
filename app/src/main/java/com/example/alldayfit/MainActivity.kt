a1663d9032ca5cce91ee00b456b821e10586ce33
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-10-19 18:07:06 +0900

    [FEAT] : ⚙️ add bottomnavigation click event - fragment

--- app/src/main/java/com/example/alldayfit/MainActivity.kt
+++ app/src/main/java/com/example/alldayfit/MainActivity.kt
@@ -2,8 +2,16 @@ package com.example.alldayfit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import androidx.fragment.app.Fragment
+import com.example.alldayfit.community.CommunityHomeFragment
+import com.example.alldayfit.databinding.ExerciseStatusFragmentBinding
 import com.example.alldayfit.databinding.MainActivityBinding
+import com.example.alldayfit.dietrecord.DietRecordFragment
+import com.example.alldayfit.exercisestatus.ExerciseStatusFragment
+import com.example.alldayfit.main.MainFragment
+import com.example.alldayfit.settings.ui.SettingMainFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: MainActivityBinding
@@ -11,12 +19,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = MainActivityBinding.inflate(layoutInflater)
         val view = mBinding.root
+        initNavigationBar()
         setContentView(view)
-        setCustomToolbar(R.id.main_toolbar)
+        setCustomToolbar()
+    }
+
+    /* bottom navigation click event - 클릭 */
+    private fun initNavigationBar() {
+        mBinding.mainNav.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.app_bar_com -> {
+                        replaceFragment(CommunityHomeFragment.newInstance())
+                    }
+
+                    R.id.app_bar_diet -> {
+                        replaceFragment(DietRecordFragment.newInstance())
+                    }
+
+                    R.id.app_bar_main -> {
+                        replaceFragment(MainFragment.newInstance())
+                    }
+
+                    R.id.app_bar_mypage -> {
+                        replaceFragment(ExerciseStatusFragment.newInstance())
+                    }
+
+                    R.id.app_bar_menu -> {
+                        replaceFragment(SettingMainFragment.newInstance())
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.app_bar_main
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val nFragment = supportFragmentManager.findFragmentById(mBinding.mainFrame.id)
+//        if (fragment) {
+//            Log.d("test","null")
+//        }
+        supportFragmentManager.beginTransaction().apply {
+            replace(mBinding.mainFrame.id, fragment)
+            commit()
+        }
     }
 
     // 툴바 메뉴 버튼을 설정- menu에 있는 item을 연결하는 부분
-    fun setCustomToolbar(layout: Int) {
+    fun setCustomToolbar() {
         val toolbar = mBinding.mainToolbar
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
