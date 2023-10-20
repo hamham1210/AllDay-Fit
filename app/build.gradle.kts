4749dd5ad434f405028501244bfc1da44e94047f
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-10-20 15:39:39 +0900

    [FEAT] : ⚙️ connect with firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id ("kotlin-kapt")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,12 +52,23 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.1")
+    // add databinding library
+    implementation("androidx.databinding:databinding-runtime:8.1.2")
 
     // chart library::MPAndroidChart. https://github.com/PhilJay/MPAndroidChart.
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     // calendar library::Calendar. https://github.com/kizitonwose/Calendar.
-    implementation ("com.kizitonwose.calendar:view:2.4.0")
+    implementation("com.kizitonwose.calendar:view:2.4.0")
+    // The compose calendar library
+    implementation("com.kizitonwose.calendar:compose:2.4.0")
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 
+    // test library
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
