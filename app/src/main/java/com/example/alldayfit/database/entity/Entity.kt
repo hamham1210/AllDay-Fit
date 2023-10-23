7b627d17e5ceff5a42651d61405dbb690e504a83
iWantBigMan <skarndgus8899@gmail.com>
2023-10-23 20:38:11 +0900

    [FEAT] : ⚙️table schema 정의

---
+++ app/src/main/java/com/example/alldayfit/database/entity/Entity.kt
@@ -0,0 +1,45 @@
+package com.example.alldayfit.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class UserEntity(
+    @PrimaryKey
+    val userId: String,
+    val email: String,
+    val name: String,
+    val nickname: String
+)
+
+@Entity(tableName = "physical_information")
+data class PhysicalInformationEntity(
+    @PrimaryKey
+    val userId: String,
+    val inputDate: String, // yyyy.MM.dd 형식의 날짜 문자열
+    val height: Int,
+    val weight: Int
+)
+
+@Entity(tableName = "exercise_logs")
+data class ExerciseLogEntity(
+    @PrimaryKey
+    val userId: String,
+    val totalTime: Int,
+    val exerciseDate: String // yyyy.MM.dd 형식의 날짜 문자열
+)
+
+@Entity(tableName = "diet_logs")
+data class DietLogEntity(
+    @PrimaryKey
+    val userId: String,
+    val breakfastFoodText: List<String>,
+    val breakfastFoodImage: String,
+    val lunchFoodText: List<String>,
+    val lunchFoodImage: String,
+    val dinnerFoodText: List<String>,
+    val dinnerFoodImage: String,
+    val snackFoodText: List<String>,
+    val snackFoodImage: String,
+    val mealDate: String // yyyy.MM.dd 형식의 날짜 문자열
+)
