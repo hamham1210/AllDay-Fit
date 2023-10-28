bcbf3f98cdce0a64e44c424571dee1f275751fc1
ljmin94 <ljmin94@naver.com>
2023-10-28 17:50:38 +0900

    식단 기록 - 하루 섭취 영양 차트로 표시(더미데이터 생성)

--- app/src/main/java/com/example/alldayfit/dietrecord/DietRecordFragment.kt
+++ app/src/main/java/com/example/alldayfit/dietrecord/DietRecordFragment.kt
@@ -9,16 +9,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.alldayfit.R
 import com.example.alldayfit.databinding.DietRecordAddDialogBinding
 import com.example.alldayfit.databinding.DietRecordFragmentBinding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 
 class DietRecordFragment : Fragment() {
     private var _binding: DietRecordFragmentBinding? = null
     private val binding get() = _binding!!
     private val dietRecordsList: MutableList<String> = mutableListOf()
+    private lateinit var dietRecordChart: BarChart
 //    private lateinit var recyclerView: RecyclerView
 //    private lateinit var dietRecordAdapter: DietRecordAdapter
 
@@ -32,7 +39,54 @@ class DietRecordFragment : Fragment() {
     ): View {
         _binding = DietRecordFragmentBinding.inflate(inflater, container, false)
         initView()
+
+        dietRecordChart = binding.dietRecordChart
+
+        val carbDataSet = BarDataSet(getBarEntries("탄수화물"), "탄수화물")
+        val proteinDataSet = BarDataSet(getBarEntries("단백질"), "단백질")
+        val fatDataSet = BarDataSet(getBarEntries("지방"), "지방")
+        val caloriesDataSet = BarDataSet(getBarEntries("칼로리"), "칼로리")
+
+        carbDataSet.color = ContextCompat.getColor(requireContext(), R.color.green)
+        proteinDataSet.color = ContextCompat.getColor(requireContext(), R.color.yellow)
+        fatDataSet.color = ContextCompat.getColor(requireContext(), R.color.black)
+        caloriesDataSet.color = ContextCompat.getColor(requireContext(), R.color.dark_blue)
+
+        val data = BarData(carbDataSet, proteinDataSet, fatDataSet, caloriesDataSet)
+        dietRecordChart.data = data
+
+        data.barWidth = 0.15f
+
+        dietRecordChart.description.isEnabled = false
+        dietRecordChart.setDrawGridBackground(false)
+
+        val xAxis = dietRecordChart.xAxis
+        xAxis.valueFormatter = IndexAxisValueFormatter(arrayOf("탄수화물", "단백질", "지방", "칼로리"))
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+        dietRecordChart.groupBars(0f, 0.05f, 0.05f)
+
+        dietRecordChart.invalidate()
+
         return binding.root
+    }
+
+    private fun getBarEntries(s: String): List<BarEntry> {
+        val entries = ArrayList<BarEntry>()
+        val random = java.util.Random()
+
+        // 하루 식단 기록에 대한 더미 데이터 생성
+        val carbs = random.nextFloat() * 100 // 탄수화물
+        val protein = random.nextFloat() * 100 // 단백질
+        val fat = random.nextFloat() * 100 // 지방
+        val calories = (carbs * 4 + protein * 4 + fat * 9).toFloat() // 칼로리 계산
+
+       entries.add(BarEntry(0f, carbs))
+        entries.add(BarEntry(1f, protein))
+        entries.add(BarEntry(2f, fat))
+        entries.add(BarEntry(3f, calories))
+
+        return entries
     }
 
     private fun initView() = with(binding) {
@@ -40,10 +94,10 @@ class DietRecordFragment : Fragment() {
         lunchView.mealTxt.text = getString(R.string.diet_record_lunch)
         dinnerView.mealTxt.text = getString(R.string.diet_record_dinner)
         snackView.mealTxt.text = getString(R.string.diet_record_snack)
-        carbohydratesView.analysisTxt.text = getString(R.string.carbohydrates)
-        proteinView.analysisTxt.text = getString(R.string.protein)
-        fatView.analysisTxt.text = getString(R.string.fat)
-        caloriesView.analysisTxt.text = getString(R.string.calories)
+//        carbohydratesView.analysisTxt.text = getString(R.string.carbohydrates)
+//        proteinView.analysisTxt.text = getString(R.string.protein)
+//        fatView.analysisTxt.text = getString(R.string.fat)
+//        caloriesView.analysisTxt.text = getString(R.string.calories)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
