d67ea0a0764246ad638ea99a5387a25fd489b642
iWantBigMan <skarndgus8899@gmail.com>
2023-10-29 05:23:26 +0900

    [FEAT] : ⚙️ 다이얼로그 프래그먼트 입력 값 뷰모델

---
+++ app/src/main/java/com/example/alldayfit/exercisestatus/BodyStatusViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.alldayfit.exercisestatus
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.alldayfit.exercisestatus.DailyEdit
+
+class BodyStatusViewModel : ViewModel() {
+
+private val _bodyStatus = MutableLiveData<BodyStatusEdit>()
+    val bodyStatus: LiveData<BodyStatusEdit>
+        get() = _bodyStatus
+
+    fun setBodyStatus(bodyStatusEdit: BodyStatusEdit) {
+        _bodyStatus.value = bodyStatusEdit
+    }
+}
