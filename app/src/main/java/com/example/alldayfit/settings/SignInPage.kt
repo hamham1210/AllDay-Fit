927176591159a0554016790d13595acdddc5dff4
phr08 <thwn1012@gmail.com>
2023-10-30 19:39:19 +0900

    [DESIGN] : 🎨 로그인 페이지 UI구성 및 관련 파일 추가

---
+++ app/src/main/java/com/example/alldayfit/settings/SignInPage.kt
@@ -0,0 +1,12 @@
+package com.example.alldayfit.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.alldayfit.R
+
+class SignInPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_page)
+    }
+}
