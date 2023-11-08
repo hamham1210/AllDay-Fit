4ae4ac72076216ffae8bc26df541e3fa5ffa19c4
ryuyeonjoo <yeonjoo3751@gmail.com>
2023-11-08 15:24:55 +0900

    휴식 countdown 기능 구현

--- app/src/main/java/com/example/alldayfit/exercisestatus/ExerciseStatusDailyEditDialog.kt
+++ app/src/main/java/com/example/alldayfit/exercisestatus/ExerciseStatusDailyEditDialog.kt
@@ -26,12 +26,12 @@ class ExerciseStatusDailyEditDialog : DialogFragment() {
     /* dialog design, data 초기 설정 */
     private fun initView() = with(bindng) {
         /*  아이템 text 초기 설정*/
-        statusWeightView.statusTypeTxt.text = getString(R.string.weight)
-        statusHeightView.statusTypeTxt.text = getString(R.string.height)
-        statusBmiView.statusTypeTxt.text = getString(R.string.bmi)
-        statusExerciseTimeView.statusTypeTxt.text = getString(R.string.exercise_time)
-        statusCalorieConsumptionView.statusTypeTxt.text = getString(R.string.calorie_consumption)
-        statusCalorieConsumptionView.statusTypeTxt.textSize = 13F
+//        statusWeightView.statusTypeTxt.text = getString(R.string.weight)
+//        statusHeightView.statusTypeTxt.text = getString(R.string.height)
+//        statusBmiView.statusTypeTxt.text = getString(R.string.bmi)
+//        statusExerciseTimeView.statusTypeTxt.text = getString(R.string.exercise_time)
+//        statusCalorieConsumptionView.statusTypeTxt.text = getString(R.string.calorie_consumption)
+//        statusCalorieConsumptionView.statusTypeTxt.textSize = 13F
 
         // dialog 닫는 버튼
         closeBtn.setOnClickListener {
