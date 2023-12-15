338d031694bbcf303cde081ed2ccc2d37cc45309
Hong Hyun Min <jjol_twi@Hongui-MacBookPro.local>
2023-12-15 17:33:10 +0900

    [CHORE] :  version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "nrplh.Iridescent.alldayfit"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 3
         versionName = "0.3"
 
@@ -48,12 +48,13 @@ android {
 }
 
 dependencies {
+    //noinspection GradleDependency
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
 
     // add databinding library
     implementation("androidx.databinding:databinding-runtime:8.1.2")
@@ -73,12 +74,15 @@ dependencies {
     implementation("com.google.firebase:firebase-database-ktx")
     // firebase cloud database
     implementation("com.google.firebase:firebase-storage-ktx")
+    // firebase authentication
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
+    // google account login
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
-    implementation("com.google.firebase:firebase-messaging:23.3.1")
+    //
+    implementation("com.google.firebase:firebase-messaging:23.4.0")
 
     // Import Room Library
-    val roomVersion = "2.6.0"
+    val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
     // To use Kotlin annotation processing tool (kapt)
@@ -95,8 +99,8 @@ dependencies {
     implementation("androidx.room:room-guava:$roomVersion")
     // optional - Paging 3 Integration
     implementation("androidx.room:room-paging:$roomVersion")
-    // navigation library
-    val navVersion = "2.7.5"
+    // navigation library ktx version
+    val navVersion = "2.7.6"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
     // Feature module Support
@@ -110,13 +114,15 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.navigation:navigation-testing:$navVersion")
 
-    //glide
+    // glide
     implementation("com.github.bumptech.glide:glide:4.13.1")
     annotationProcessor("com.github.bumptech.glide:compiler:4.13.1")
 
-    //coil
+    // coil
     implementation("io.coil-kt:coil:2.5.0")
 
-    //lottie
+    // lottie
     implementation("com.airbnb.android:lottie:6.1.0")
+
+
 }
